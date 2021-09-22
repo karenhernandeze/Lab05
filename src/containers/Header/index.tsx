e21e0ca1a9265cbf5bdadc96c3623295f8357c05
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-09-21 22:32:50 -0600

    Challenge 2

---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigationBar from "../../components/Navbar"; 
+
+class Header extends React.Component {
+    
+    render() {
+        return (
+            <NavigationBar/>
+        )
+    }
+}
+
+export default Header; 
